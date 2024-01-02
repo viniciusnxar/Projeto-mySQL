b6fcea29f3cf9bde389423a385100ec244a08a4b
Vinicius Noronha <96258593+viniciusnxar@users.noreply.github.com>
2024-01-02 16:46:26 -0300

    Update Relatorio 2.sql

--- Relatórios/Relatorio 2.sql
+++ Relatórios/Relatorio 2.sql
@@ -1,18 +1,24 @@
-select tbl_funcionario.estado_civil,
-tbl_funcionario.doadorOrgaos,
-CONCAT(tbl_dependentes.primeiroNomeDependente, " ", tbl_dependentes.primeiroNomeDependente) AS nomeDependente,
-CONCAT(tbl_funcionario.primeiroNomeFuncionario, " ", tbl_funcionario.segundoNomeFuncionario) AS nome,
-tbl_exames.valorReferencia,
-tbl_laudo.resultado,
-case
-when tbl_exames.idExame = 3 then 
-(select tbl_exames.nomeExame from tbl_exames as e where e.idFuncionario = tbl_funcionario.idFuncionario and  e.dataExame = (select max(ex.dataExame) from tbl_exames as ex where ex.idExame = 3 and ex.idFuncionario = tbl_funcionario.idFuncionario GROUP BY ex.idExame, ex.idFuncionario))
-else tbl_exames.nomeExame
-
-
-end AS ultimoExame
-
-from tbl_funcionario
-LEFT JOIN tbl_dependentes on tbl_funcionario.idFuncionario = tbl_dependentes.idFuncionario
-LEFT JOIN tbl_exames on tbl_funcionario.idFuncionario = tbl_exames.idFuncionario and tbl_exames.idExame
-LEFT JOIN tbl_laudo on tbl_funcionario.idFuncionario = tbl_exames_idExame;
+SELECT
+    tbl_funcionario.estado_civil,
+    tbl_funcionario.doadorOrgaos,
+    CONCAT(tbl_dependentes.primeiroNomeDependente, ' ', tbl_dependentes.segundoNomeDependente) AS nomeDependente,
+    CONCAT(tbl_funcionario.primeiroNomeFuncionario, ' ', tbl_funcionario.segundoNomeFuncionario) AS nome,
+    tbl_exames.valorReferencia,
+    tbl_laudo.resultado,
+    CASE
+        WHEN tbl_exames.idExame = 3 THEN
+            (SELECT tbl_exames.nomeExame
+             FROM tbl_exames AS e
+             WHERE e.idFuncionario = tbl_funcionario.idFuncionario
+               AND e.dataExame = (SELECT MAX(ex.dataExame)
+                                  FROM tbl_exames AS ex
+                                  WHERE ex.idExame = 3
+                                    AND ex.idFuncionario = tbl_funcionario.idFuncionario
+                                  GROUP BY ex.idExame, ex.idFuncionario))
+        ELSE tbl_exames.nomeExame
+    END AS ultimoExame
+FROM
+    tbl_funcionario
+LEFT JOIN tbl_dependentes ON tbl_funcionario.idFuncionario = tbl_dependentes.idFuncionario
+LEFT JOIN tbl_exames ON tbl_funcionario.idFuncionario = tbl_exames.idFuncionario AND tbl_exames.idExame
+LEFT JOIN tbl_laudo ON tbl_funcionario.idFuncionario = tbl_exames.idExame;
